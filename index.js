98230fb2f753119e8041bbdc1a6499cfbcc179d2
Natthawat Pomsuwan <126516264+64332110222-6@users.noreply.github.com>
2024-01-12 22:36:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const path = require('path')
-const router = jsonServer.router(path.join(__dirname, './db/todo.json'))
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("todo.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
+server.use(middlewares);
+server.use(router);
 
-server.use(jsonServer.defaults())
-server.use(router)
-server.listen(8000, () => {
-  console.log('JSON Server is running')
-})
+server.listen(port);
